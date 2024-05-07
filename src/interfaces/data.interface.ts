cfb16920f167ff1806261a2df333a457293fbf93
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-07 14:32:48 -0400

    complet interface initialPokemonState

--- src/interfaces/data.interface.ts
+++ src/interfaces/data.interface.ts
@@ -36,7 +36,156 @@ export const initialPokemonState: IPokemon = {
       home: { front_default: '', front_shiny: '', front_shiny_female: undefined },
       'official-artwork': { front_default: '' }
     },
-    versions: {}
+    versions: {
+      'generation-i': {
+        'red-blue': {
+          back_default: '',
+          back_gray: '',
+          back_transparent: '',
+          front_default: '',
+          front_gray: '',
+          front_transparent: ''
+        },
+        yellow: {
+          back_default: '',
+          back_gray: '',
+          back_transparent: '',
+          front_default: '',
+          front_gray: '',
+          front_transparent: ''
+        }
+      },
+      'generation-ii': {
+        crystal: {
+          back_default: '',
+          back_shiny: '',
+          back_shiny_transparent: '',
+          back_transparent: '',
+          front_default: '',
+          front_shiny: '',
+          front_shiny_transparent: '',
+          front_transparent: ''
+        },
+        gold: {
+          back_default: '',
+          back_shiny: '',
+          front_default: '',
+          front_shiny: '',
+          front_transparent: ''
+        },
+        silver: {
+          back_default: '',
+          back_shiny: '',
+          front_default: '',
+          front_shiny: '',
+          front_transparent: ''
+        }
+      },
+      'generation-iii': {
+        emerald: {
+          front_default: '',
+          front_shiny: ''
+        },
+        'firered-leafgreen': {
+          back_default: '',
+          back_shiny: '',
+          front_default: '',
+          front_shiny: ''
+        },
+        'ruby-sapphire': {
+          back_default: '',
+          back_shiny: '',
+          front_default: '',
+          front_shiny: ''
+        }
+      },
+      'generation-iv': {
+        'diamond-pearl': {
+          back_default: '',
+          back_shiny: '',
+          back_shiny_female: undefined,
+          back_female: undefined,
+          front_default: '',
+          front_shiny: '',
+          front_shiny_female: undefined,
+          front_female: undefined
+        },
+        'heartgold-soulsilver': {
+          back_default: '',
+          back_shiny: '',
+          back_shiny_female: undefined,
+          back_female: undefined,
+          front_default: '',
+          front_shiny: '',
+          front_shiny_female: undefined,
+          front_female: undefined
+        },
+        platinum: {
+          back_default: '',
+          back_shiny: '',
+          back_shiny_female: undefined,
+          back_female: undefined,
+          front_default: '',
+          front_shiny: '',
+          front_shiny_female: undefined,
+          front_female: undefined
+        }
+      },
+      'generation-v': {
+        'black-white': {
+          animated: {
+            back_default: '',
+            back_shiny: '',
+            back_shiny_female: undefined,
+            back_female: undefined,
+            front_default: '',
+            front_shiny: '',
+            front_shiny_female: undefined,
+            front_female: undefined
+          },
+          back_default: '',
+          back_shiny: '',
+          back_shiny_female: undefined,
+          back_female: undefined,
+          front_default: '',
+          front_shiny: '',
+          front_shiny_female: undefined,
+          front_female: undefined
+        }
+      },
+      'generation-vi': {
+        'omegaruby-alphasapphire': {
+          front_default: '',
+          front_female: undefined,
+          front_shiny: '',
+          front_shiny_female: undefined
+        },
+        'x-y': {
+          front_default: '',
+          front_female: undefined,
+          front_shiny: '',
+          front_shiny_female: undefined
+        }
+      },
+      'generation-vii': {
+        icons: {
+          front_default: '',
+          front_female: undefined
+        },
+        'ultra-sun-ultra-moon': {
+          front_default: '',
+          front_shiny: '',
+          front_shiny_female: undefined,
+          front_female: undefined
+        }
+      },
+      'generation-viii': {
+        icons: {
+          front_default: '',
+          front_female: undefined
+        }
+      }
+    }
   },
   stats: [],
   types: [],
